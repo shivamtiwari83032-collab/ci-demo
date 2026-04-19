0d0845d26a99b981015d12cfea7a0f972015994d
Shivam Tiwari <shivamtiwari8302@gmail.com>
2026-04-19 07:44:11 +0530

    add test logic with no error

--- app.py
+++ app.py
@@ -5,14 +5,10 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
 
-
-result1 = add(4, 5)
-result2 = sub(4, 1)
-
-print(result1, result2)
 
 if __name__ == '__main__':
     print("Addition:", add(4, 5))
